745635a73022abeecfd750d24734e64fa29aa4a4
Steven Templeman <templeman15@hotmail.com>
2017-11-08 19:50:04 -0800

    Init commit

---
+++ SwiftCodersGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCodersGit
+//
+//  Created by Steven Templeman on 11/8/17.
+//  Copyright © 2017 Coplex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
