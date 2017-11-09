bf3c506b42311d84e8e66666a539b3a2af735374
Steven Templeman <templeman15@hotmail.com>
2017-11-08 20:25:19 -0800

    Added addNumbers() to ViewController

--- SwiftCodersGit/ViewController.swift
+++ SwiftCodersGit/ViewController.swift
@@ -11,10 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        _ = addNumbers()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addNumbers() -> Int {
+        return 2 + 2
     }
 }
